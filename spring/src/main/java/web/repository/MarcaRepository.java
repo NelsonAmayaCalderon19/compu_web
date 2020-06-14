41c448df5215bf26bfb1dea877448fef14d33de1
NelsonAmayaCalderon19 <nelsoneduardoac@ufps.edu.co>
2020-06-14 17:34:10 -0500

    no message

--- spring/src/main/java/web/repository/MarcaRepository.java
+++
@@ -1,9 +0,0 @@
-package web.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import web.entities.Marca;
-
-public interface MarcaRepository extends JpaRepository<Marca, Integer> {
-
-}
